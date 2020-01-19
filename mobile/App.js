55f8a4e7c48f7db78e91a6a04e39637218f15bcc
josephdsbr <josephdsbr@gmail.com>
2020-01-18 22:59:02 -0300

    Finalizando aplicação mobile

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routes from './src/Routes';
+import {StatusBar} from 'react-native';
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routes />
+    </>
+  );
+}
